384ebe8e1f801c7202144c75b57d5bc5f82446c3
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-16 20:11:15 -0400

    feat(core-driver): define driver provider interface

---
+++ src/main/java/com/ovidiomiranda/framework/core/providers/DriverProvider.java
@@ -0,0 +1,20 @@
+package com.ovidiomiranda.framework.core.providers;
+
+import io.appium.java_client.AppiumDriver;
+
+/**
+ * Defines how a mobile driver should be created.
+ *
+ * <p>Each platform must implement this interface.</p>
+ *
+ * @author Ovidio Miranda
+ */
+public interface DriverProvider {
+
+  /**
+   * Creates and returns a mobile driver.
+   *
+   * @return AppiumDriver instance
+   */
+  AppiumDriver getDriver();
+}
