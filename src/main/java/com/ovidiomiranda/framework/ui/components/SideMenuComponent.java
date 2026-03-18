178a487c8cd4b5ef45a65ef6f456f179938580bf
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-18 19:13:31 -0400

    refactor(ui): inject MobileElementActions into SideMenuComponent

--- src/main/java/com/ovidiomiranda/framework/ui/components/SideMenuComponent.java
+++ src/main/java/com/ovidiomiranda/framework/ui/components/SideMenuComponent.java
@@ -1,5 +1,8 @@
 package com.ovidiomiranda.framework.ui.components;
 
+import static com.ovidiomiranda.framework.ui.enums.MenuOption.LOGIN;
+import static com.ovidiomiranda.framework.ui.enums.MenuOption.LOGOUT;
+
 import com.ovidiomiranda.framework.core.interactions.MobileElementActions;
 import com.ovidiomiranda.framework.ui.enums.MenuOption;
 import io.appium.java_client.AppiumBy;
@@ -10,29 +13,42 @@ import org.openqa.selenium.By;
 /**
  * Represents the side navigation menu (hamburger menu).
  *
- * <p>This component is reusable across different pages</p>.
+ * <p>Provides methods to open the menu and navigate using menu options. Reusable across pages.</p>
  *
  * @author Ovidio Miranda
  */
 public class SideMenuComponent {
 
+  private final MobileElementActions actions;
   private final By hamburgerButton = AppiumBy.id("com.saucelabs.mydemoapp.android:id/menuIV");
   private static final Map<MenuOption, By> MENU_OPTIONS = new EnumMap<>(MenuOption.class);
 
   static {
-    MENU_OPTIONS.put(MenuOption.LOGIN, AppiumBy.accessibilityId("Login Menu Item"));
-    MENU_OPTIONS.put(MenuOption.LOGOUT, AppiumBy.accessibilityId("Logout Menu Item"));
+    MENU_OPTIONS.put(LOGIN, AppiumBy.accessibilityId("Login Menu Item"));
+    MENU_OPTIONS.put(LOGOUT, AppiumBy.accessibilityId("Logout Menu Item"));
   }
 
   /**
-   * Opens the side menu.
+   * Constructor.
+   *
+   * @param actions MobileElementActions instance for element interaction
+   */
+  public SideMenuComponent(MobileElementActions actions) {
+    this.actions = actions;
+  }
+
+  /**
+   * Opens the side menu by tapping the hamburger button.
    */
   public void openMenu() {
-    MobileElementActions.tap(hamburgerButton);
+    actions.tap(hamburgerButton);
   }
 
   /**
-   * Navigates using menu option.
+   * Navigates to a menu option.
+   *
+   * @param option MenuOption to navigate to
+   * @throws IllegalArgumentException if menu option is invalid
    */
   public void navigateTo(MenuOption option) {
     openMenu();
@@ -40,6 +56,6 @@ public class SideMenuComponent {
     if (locator == null) {
       throw new IllegalArgumentException("Invalid menu option: " + option);
     }
-    MobileElementActions.tap(locator);
+    actions.tap(locator);
   }
 }
