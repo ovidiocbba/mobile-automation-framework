cfa261c38215269533f789f6319478da13ae1af0
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-17 22:14:22 -0400

    feat(ui): implement ProductsPage with visibility validation

---
+++ src/main/java/com/ovidiomiranda/framework/ui/pages/ProductsPage.java
@@ -0,0 +1,25 @@
+package com.ovidiomiranda.framework.ui.pages;
+
+import com.ovidiomiranda.framework.core.interactions.MobileElementActions;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+
+/**
+ * Represents the 'Product' page and its components.
+ *
+ * @author Ovidio Miranda
+ */
+public class ProductsPage extends BasePage {
+
+  private final By productsTitle = AppiumBy.id("com.saucelabs.mydemoapp.android:id/productTV");
+
+
+  /**
+   * Verifies if Products page is displayed.
+   *
+   * @return true if page is visible
+   */
+  public boolean isDisplayed() {
+    return MobileElementActions.isDisplayed(productsTitle);
+  }
+}
