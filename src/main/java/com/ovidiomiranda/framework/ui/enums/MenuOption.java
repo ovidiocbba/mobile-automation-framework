37e4dbc276dc4cdd9dbb0a22bccad72cecdb2777
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-17 22:12:24 -0400

    feat(ui): add MenuOption enum for side menu navigation

---
+++ src/main/java/com/ovidiomiranda/framework/ui/enums/MenuOption.java
@@ -0,0 +1,17 @@
+package com.ovidiomiranda.framework.ui.enums;
+
+/**
+ * Represents available options in the side menu.
+ *
+ * @author Ovidio Miranda
+ */
+public enum MenuOption {
+  /**
+   * Navigates to 'Login' screen
+   */
+  LOGIN,
+  /**
+   * Logs out the current user
+   */
+  LOGOUT
+}
