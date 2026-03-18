692971b7f34d71f14824861e17d9f398eedfee15
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-18 19:14:29 -0400

    refactor(ui): use actions instance and inject dependencies via constructor

--- src/main/java/com/ovidiomiranda/framework/ui/pages/ProductsPage.java
+++ src/main/java/com/ovidiomiranda/framework/ui/pages/ProductsPage.java
@@ -1,5 +1,6 @@
 package com.ovidiomiranda.framework.ui.pages;
 
+import com.ovidiomiranda.framework.core.driver.DriverContext;
 import com.ovidiomiranda.framework.core.interactions.MobileElementActions;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
@@ -13,13 +14,22 @@ public class ProductsPage extends BasePage {
 
   private final By productsTitle = AppiumBy.id("com.saucelabs.mydemoapp.android:id/productTV");
 
+  /**
+   * Constructor.
+   *
+   * @param driverContext driver context
+   * @param actions       MobileElementActions utility
+   */
+  public ProductsPage(DriverContext driverContext, MobileElementActions actions) {
+    super(driverContext, actions);
+  }
 
   /**
    * Verifies if Products page is displayed.
    *
-   * @return true if page is visible
+   * @return true if page is visible, false otherwise
    */
   public boolean isDisplayed() {
-    return MobileElementActions.isDisplayed(productsTitle);
+    return actions.isDisplayed(productsTitle);
   }
 }
