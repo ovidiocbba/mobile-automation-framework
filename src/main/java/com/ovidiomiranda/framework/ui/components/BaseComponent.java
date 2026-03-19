58a1ed248a28c511e9996dc844a708af8452b4f4
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-19 15:26:59 -0400

    feat(core-components): add BaseComponent with locator resolution and shared actions

---
+++ src/main/java/com/ovidiomiranda/framework/ui/components/BaseComponent.java
@@ -0,0 +1,60 @@
+package com.ovidiomiranda.framework.ui.components;
+
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.PLATFORM;
+
+import com.ovidiomiranda.framework.core.config.ConfigValidator;
+import com.ovidiomiranda.framework.core.driver.DriverContext;
+import com.ovidiomiranda.framework.core.enums.PlatformType;
+import com.ovidiomiranda.framework.core.interactions.MobileElementActions;
+import com.ovidiomiranda.framework.core.locators.MobileLocator;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+
+/**
+ * Base class for all UI elements (pages and components).
+ *
+ * <p>Provides common functionality like locator resolution and element interactions.</p>
+ *
+ * @author Ovidio Miranda
+ */
+public abstract class BaseComponent {
+
+  protected final ConfigValidator config;
+  protected final DriverContext driverContext;
+  protected final MobileElementActions actions;
+
+  /**
+   * Constructor.
+   *
+   * @param config        configuration validator
+   * @param driverContext driver context
+   * @param actions       mobile actions utility
+   */
+  protected BaseComponent(ConfigValidator config, DriverContext driverContext,
+      MobileElementActions actions) {
+    this.config = config;
+    this.driverContext = driverContext;
+    this.actions = actions;
+  }
+
+  /**
+   * Gets the current AppiumDriver instance.
+   *
+   * @return active AppiumDriver
+   */
+  protected AppiumDriver getDriver() {
+    return driverContext.getDriver();
+  }
+
+  /**
+   * Resolves a MobileLocator into a platform-specific locator.
+   *
+   * @param locator mobile locator
+   * @return resolved locator for current platform
+   */
+  protected By resolve(MobileLocator locator) {
+    String platform = config.require(PLATFORM);
+    PlatformType platformType = PlatformType.valueOf(platform.toUpperCase());
+    return locator.get(platformType);
+  }
+}
