668a60a0526f13138b18de61cda388ebf267b114
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-19 15:28:42 -0400

    refactor(ui-pages): migrate ProductsPage to MobileLocator

--- src/main/java/com/ovidiomiranda/framework/ui/pages/ProductsPage.java
+++ src/main/java/com/ovidiomiranda/framework/ui/pages/ProductsPage.java
@@ -1,9 +1,10 @@
 package com.ovidiomiranda.framework.ui.pages;
 
+import com.ovidiomiranda.framework.core.config.ConfigValidator;
 import com.ovidiomiranda.framework.core.driver.DriverContext;
 import com.ovidiomiranda.framework.core.interactions.MobileElementActions;
+import com.ovidiomiranda.framework.core.locators.MobileLocator;
 import io.appium.java_client.AppiumBy;
-import org.openqa.selenium.By;
 
 /**
  * Represents the 'Product' page and its components.
@@ -12,16 +13,26 @@ import org.openqa.selenium.By;
  */
 public class ProductsPage extends BasePage {
 
-  private final By productsTitle = AppiumBy.id("productTV");
+  private final MobileLocator productsTitle = new MobileLocator(AppiumBy.id("productTV"),
+      AppiumBy.accessibilityId("products_title"));
+
+  private final MobileLocator firstProduct = new MobileLocator(
+      AppiumBy.xpath("(//android.widget.ImageView[@content-desc='Product Image'])[1]"),
+      AppiumBy.xpath("(//XCUIElementTypeImage[@name='Product Image'])[1]"));
+
+  private final MobileLocator cartBadge = new MobileLocator(AppiumBy.id("cartTV"),
+      AppiumBy.xpath("//XCUIElementTypeStaticText[@name='cart_badge']"));
 
   /**
    * Constructor.
    *
+   * @param config        config validator
    * @param driverContext driver context
    * @param actions       MobileElementActions utility
    */
-  public ProductsPage(DriverContext driverContext, MobileElementActions actions) {
-    super(driverContext, actions);
+  public ProductsPage(ConfigValidator config, DriverContext driverContext,
+      MobileElementActions actions) {
+    super(config, driverContext, actions);
   }
 
   /**
@@ -30,9 +41,7 @@ public class ProductsPage extends BasePage {
    * <p>This method opens the product detail screen for the first available product.</p>
    */
   public void selectFirstProduct() {
-    By firstProduct = AppiumBy.xpath(
-        "(//android.widget.ImageView[@content-desc='Product Image'])[1]");
-    actions.tap(firstProduct);
+    actions.tap(resolve(firstProduct));
   }
 
   /**
@@ -44,8 +53,7 @@ public class ProductsPage extends BasePage {
    * @return cart item count as text
    */
   public String getCartBadgeCount() {
-    By cartBadge = AppiumBy.id("cartTV");
-    return actions.getText(cartBadge);
+    return actions.getText(resolve(cartBadge));
   }
 
   /**
@@ -54,6 +62,6 @@ public class ProductsPage extends BasePage {
    * @return true if page is visible, false otherwise
    */
   public boolean isDisplayed() {
-    return actions.isDisplayed(productsTitle);
+    return actions.isDisplayed(resolve(productsTitle));
   }
 }
