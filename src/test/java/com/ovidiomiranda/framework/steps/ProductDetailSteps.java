cf3c1cb194dc0c74539b4fa1080b279ecd64eae1
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-20 16:57:00 -0400

    feat(steps): add validations for product name and price in detail page

---
+++ src/test/java/com/ovidiomiranda/framework/steps/ProductDetailSteps.java
@@ -0,0 +1,42 @@
+package com.ovidiomiranda.framework.steps;
+
+import com.ovidiomiranda.framework.ui.pages.ProductDetailPage;
+import io.cucumber.java.en.Then;
+import org.testng.Assert;
+
+/**
+ * Step definitions related to actions performed on the 'Product Detail' page.
+ *
+ * @author Ovidio Miranda
+ */
+public class ProductDetailSteps {
+
+  private final ProductDetailPage productDetailPage;
+
+  /**
+   * Constructor.
+   *
+   * @param productDetailPage product detail page object
+   */
+  public ProductDetailSteps(ProductDetailPage productDetailPage) {
+    this.productDetailPage = productDetailPage;
+  }
+
+  /**
+   * Verifies the product name in the detail screen.
+   */
+  @Then("the product name should be {string}")
+  public void verifyProductName(String expectedName) {
+    String actual = productDetailPage.getProductName();
+    Assert.assertEquals(actual, expectedName, "Product name mismatch");
+  }
+
+  /**
+   * Verifies the product price in the detail screen.
+   */
+  @Then("the product price should be {string}")
+  public void verifyProductPrice(String expectedPrice) {
+    String actual = productDetailPage.getProductPrice();
+    Assert.assertEquals(actual, expectedPrice, "Product price mismatch");
+  }
+}
