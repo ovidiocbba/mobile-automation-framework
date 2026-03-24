f6db0ff02690c8162f8bd2c297b7a36d2a819864
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-24 14:10:31 -0400

    style(menu-navigation): apply spotless formatting

--- src/main/java/com/ovidiomiranda/framework/ui/navigation/MenuNavigation.java
+++ src/main/java/com/ovidiomiranda/framework/ui/navigation/MenuNavigation.java
@@ -7,7 +7,7 @@ import com.ovidiomiranda.framework.ui.components.SideMenuComponent;
 /**
  * Handles navigation through the app menu.
  *
- * <p>Provides specific navigation methods to app screens using the side menu.</p>
+ * <p>Provides specific navigation methods to app screens using the side menu.
  *
  * @author Ovidio Miranda
  */
@@ -24,9 +24,7 @@ public class MenuNavigation {
     this.sideMenu = sideMenu;
   }
 
-  /**
-   * Navigates to 'Login' screen using the side menu.
-   */
+  /** Navigates to 'Login' screen using the side menu. */
   public void goToLogin() {
     sideMenu.navigateTo(LOGIN);
   }
