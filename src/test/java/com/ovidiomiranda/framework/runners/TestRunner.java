c0da324b42f0978bb8255a3d97b1207261021106
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-24 14:21:29 -0400

    style(tests): adjust formatting and Javadoc in TestRunner

--- src/test/java/com/ovidiomiranda/framework/runners/TestRunner.java
+++ src/test/java/com/ovidiomiranda/framework/runners/TestRunner.java
@@ -4,19 +4,16 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-/**
- * Test Runner class responsible for executing Cucumber feature files.
- */
+/** Test Runner class responsible for executing Cucumber feature files. */
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = "com.ovidiomiranda.framework",
     plugin = {
-        "pretty",
-        "json:build/cucumber.json",
-        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-        "rerun:build/rerun.txt"
-    }
-)
+      "pretty",
+      "json:build/cucumber.json",
+      "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+      "rerun:build/rerun.txt"
+    })
 public class TestRunner extends AbstractTestNGCucumberTests {
 
   /**
