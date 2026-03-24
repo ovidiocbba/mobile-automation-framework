756ede56d60989a7f4be762064897902041324e1
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-24 18:12:35 -0400

    feat(steps): add HeaderSteps for cart badge interactions

---
+++ src/test/java/com/ovidiomiranda/framework/steps/HeaderSteps.java
@@ -0,0 +1,42 @@
+package com.ovidiomiranda.framework.steps;
+
+import com.ovidiomiranda.framework.ui.components.HeaderComponent;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+/**
+ * Step definitions for Header component validations.
+ *
+ * @author Ovidio Miranda
+ */
+public class HeaderSteps {
+
+  private final HeaderComponent header;
+
+  /**
+   * Constructor.
+   *
+   * @param header header component instance
+   */
+  public HeaderSteps(HeaderComponent header) {
+    this.header = header;
+  }
+
+  /** Taps the 'Cart' badge. */
+  @When("I tap the 'Cart' badge")
+  public void tapCartBadge() {
+    header.tapCartBadge();
+  }
+
+  /**
+   * Verifies the 'Cart' badge count.
+   *
+   * @param expectedCount expected number of items in the cart
+   */
+  @Then("the 'Cart' badge should show {int} item")
+  public void verifyCartBadgeCount(final int expectedCount) {
+    final String actual = header.getCartBadgeCount();
+    Assert.assertEquals(actual, String.valueOf(expectedCount), "Cart count mismatch");
+  }
+}
