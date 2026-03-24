bbc134a739fede09cb9883caa68e04f2e76c50c0
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-24 14:08:13 -0400

    style(driver-provider): apply spotless formatting

--- src/main/java/com/ovidiomiranda/framework/core/providers/DriverProvider.java
+++ src/main/java/com/ovidiomiranda/framework/core/providers/DriverProvider.java
@@ -5,7 +5,7 @@ import io.appium.java_client.AppiumDriver;
 /**
  * Defines how a mobile driver should be created.
  *
- * <p>Each platform must implement this interface.</p>
+ * <p>Each platform must implement this interface.
  *
  * @author Ovidio Miranda
  */
