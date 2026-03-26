f3de67a873eb17b67a10c2de0d55ea41083dcdb7
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-26 14:59:10 -0400

    feat(core-providers): support BrowserStack and local driver initialization

--- src/main/java/com/ovidiomiranda/framework/core/providers/AndroidDriverProvider.java
+++ src/main/java/com/ovidiomiranda/framework/core/providers/AndroidDriverProvider.java
@@ -1,8 +1,14 @@
 package com.ovidiomiranda.framework.core.providers;
 
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_ACCESS_KEY;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_URL;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_USERNAME;
+
 import com.ovidiomiranda.framework.core.capabilities.AndroidCapabilities;
 import com.ovidiomiranda.framework.core.config.ConfigValidator;
+import com.ovidiomiranda.framework.core.enums.ExecutionType;
 import com.ovidiomiranda.framework.core.enums.PropertiesInput;
+import com.ovidiomiranda.framework.core.utils.ExecutionUtils;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
@@ -11,7 +17,12 @@ import java.net.URL;
 /**
  * Creates Android driver instances.
  *
- * <p>Connects to Appium server and starts a session with Android capabilities.
+ * <p>Supports:
+ *
+ * <ul>
+ *   <li>Local Appium execution
+ *   <li>BrowserStack cloud execution
+ * </ul>
  *
  * @author Ovidio Miranda
  */
@@ -36,8 +47,27 @@ public class AndroidDriverProvider implements DriverProvider {
   public AppiumDriver getDriver() {
     try {
       UiAutomator2Options capabilities = androidCapabilities.getCapabilities();
-      String appiumServerUrl = config.require(PropertiesInput.APPIUM_SERVER_URL);
-      return new AndroidDriver(new URL(appiumServerUrl), capabilities);
+      ExecutionType executionType = ExecutionUtils.getExecutionType(config);
+      String url;
+      switch (executionType) {
+        case BROWSERSTACK:
+          String baseUrl = config.require(BS_URL);
+          url =
+              baseUrl.replace(
+                  "https://",
+                  "https://"
+                      + config.require(BS_USERNAME)
+                      + ":"
+                      + config.require(BS_ACCESS_KEY)
+                      + "@");
+          break;
+
+        case LOCAL:
+        default:
+          url = config.require(PropertiesInput.APPIUM_SERVER_URL);
+          break;
+      }
+      return new AndroidDriver(new URL(url), capabilities);
     } catch (Exception e) {
       throw new RuntimeException("Failed to create Android driver", e);
     }
