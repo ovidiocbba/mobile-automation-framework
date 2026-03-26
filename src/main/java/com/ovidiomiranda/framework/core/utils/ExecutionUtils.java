3aa81e4e53e91be62a8d64ae4f8d519262b94a19
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-26 14:58:37 -0400

    feat(core-utils): add utility to resolve execution type from config

---
+++ src/main/java/com/ovidiomiranda/framework/core/utils/ExecutionUtils.java
@@ -0,0 +1,38 @@
+package com.ovidiomiranda.framework.core.utils;
+
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.EXECUTION;
+
+import com.ovidiomiranda.framework.core.config.ConfigValidator;
+import com.ovidiomiranda.framework.core.enums.ExecutionType;
+
+/**
+ * Utility class for execution type handling.
+ *
+ * <p>Reads execution configuration and determines where tests will run.
+ *
+ * @author Ovidio Miranda
+ */
+public final class ExecutionUtils {
+
+  private ExecutionUtils() {}
+
+  /**
+   * Gets execution type from configuration.
+   *
+   * @param config configuration validator
+   * @return ExecutionType
+   */
+  public static ExecutionType getExecutionType(ConfigValidator config) {
+    return ExecutionType.from(config.optional(EXECUTION));
+  }
+
+  /**
+   * Checks if execution is BrowserStack.
+   *
+   * @param config configuration validator
+   * @return true if BrowserStack
+   */
+  public static boolean isBrowserStack(ConfigValidator config) {
+    return getExecutionType(config) == ExecutionType.BROWSERSTACK;
+  }
+}
