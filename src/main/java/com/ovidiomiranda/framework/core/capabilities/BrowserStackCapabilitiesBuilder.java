fbdc754601c683ab501153f88beb78a5c1af3abc
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-27 21:48:12 -0400

    feat(browserstack): externalize project/build configuration and add extended BrowserStack capabilities

--- src/main/java/com/ovidiomiranda/framework/core/capabilities/BrowserStackCapabilitiesBuilder.java
+++ src/main/java/com/ovidiomiranda/framework/core/capabilities/BrowserStackCapabilitiesBuilder.java
@@ -2,7 +2,16 @@ package com.ovidiomiranda.framework.core.capabilities;
 
 import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_ACCESS_KEY;
 import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_APP;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_APPIUM_LOGS;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_APPIUM_VERSION;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_BUILD_NAME;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_DEVICE_LOGS;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_NETWORK_LOGS;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_PROJECT_NAME;
 import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_USERNAME;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_VIDEO;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.DEVICE_NAME;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.PLATFORM_VERSION;
 
 import com.ovidiomiranda.framework.core.config.ConfigValidator;
 import io.appium.java_client.remote.options.BaseOptions;
@@ -16,8 +25,6 @@ import java.util.Map;
  */
 public class BrowserStackCapabilitiesBuilder {
 
-  private static final String PROJECT_NAME = "Mobile Automation Framework";
-  private static final String BUILD_NAME = "Build-1";
   private final ConfigValidator config;
 
   /**
@@ -35,22 +42,30 @@ public class BrowserStackCapabilitiesBuilder {
    * <p>This method injects all required BrowserStack configuration, including authentication,
    * application reference, and session metadata.
    *
+   * @param sessionName session name used to identify the test execution in BrowserStack
    * @param options Appium options to enrich
-   * @param sessionName name of the test session
    */
   public void apply(final BaseOptions<?> options, final String sessionName) {
 
     // Set app uploaded to BrowserStack
-    options.setCapability("appium:app", config.require(BS_APP));
+    options.setCapability("app", config.require(BS_APP));
 
     // Build BrowserStack options map
     final Map<String, Object> bstackOptions = new HashMap<>();
     bstackOptions.put("userName", config.require(BS_USERNAME));
     bstackOptions.put("accessKey", config.require(BS_ACCESS_KEY));
-    bstackOptions.put("projectName", PROJECT_NAME);
-    bstackOptions.put("buildName", BUILD_NAME);
+    bstackOptions.put("projectName", config.require(BS_PROJECT_NAME));
+    bstackOptions.put("buildName", config.require(BS_BUILD_NAME));
     bstackOptions.put("sessionName", sessionName);
 
+    bstackOptions.put("deviceName", config.require(DEVICE_NAME));
+    bstackOptions.put("osVersion", config.require(PLATFORM_VERSION));
+
+    bstackOptions.put("video", Boolean.parseBoolean(config.require(BS_VIDEO)));
+    bstackOptions.put("deviceLogs", Boolean.parseBoolean(config.require(BS_DEVICE_LOGS)));
+    bstackOptions.put("appiumLogs", Boolean.parseBoolean(config.require(BS_APPIUM_LOGS)));
+    bstackOptions.put("networkLogs", Boolean.parseBoolean(config.require(BS_NETWORK_LOGS)));
+    bstackOptions.put("appiumVersion", config.require(BS_APPIUM_VERSION));
     // Attach to capabilities
     options.setCapability("bstack:options", bstackOptions);
   }
