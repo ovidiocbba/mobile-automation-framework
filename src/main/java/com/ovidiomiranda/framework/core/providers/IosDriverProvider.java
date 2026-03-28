5997969e8c217a55b58b60bebbece21f6a139e75
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-27 21:51:40 -0400

    feat(ios): support BrowserStack execution and dynamic Appium server URL resolution

--- src/main/java/com/ovidiomiranda/framework/core/providers/IosDriverProvider.java
+++ src/main/java/com/ovidiomiranda/framework/core/providers/IosDriverProvider.java
@@ -1,8 +1,16 @@
 package com.ovidiomiranda.framework.core.providers;
 
+import static com.ovidiomiranda.framework.core.enums.ExecutionType.BROWSERSTACK;
+import static com.ovidiomiranda.framework.core.enums.ExecutionType.LOCAL;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_ACCESS_KEY;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_URL;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_USERNAME;
+
 import com.ovidiomiranda.framework.core.capabilities.IosCapabilities;
 import com.ovidiomiranda.framework.core.config.ConfigValidator;
+import com.ovidiomiranda.framework.core.enums.ExecutionType;
 import com.ovidiomiranda.framework.core.enums.PropertiesInput;
+import com.ovidiomiranda.framework.core.utils.ExecutionUtils;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.options.XCUITestOptions;
@@ -33,11 +41,29 @@ public class IosDriverProvider implements DriverProvider {
 
   /** {@inheritDoc} */
   @Override
-  public AppiumDriver getDriver() {
+  public AppiumDriver getDriver(final String sessionName) {
     try {
-      final XCUITestOptions capabilities = iosCapabilities.getCapabilities();
-      final String appiumServerUrl = config.require(PropertiesInput.APPIUM_SERVER_URL);
-      return new IOSDriver(new URL(appiumServerUrl), capabilities);
+      final XCUITestOptions capabilities = iosCapabilities.getCapabilities(sessionName);
+      final ExecutionType executionType = ExecutionUtils.getExecutionType(config);
+      final String url;
+      switch (executionType) {
+        case BROWSERSTACK:
+          final String baseUrl = config.require(BS_URL);
+          url =
+              baseUrl.replace(
+                  "https://",
+                  "https://"
+                      + config.require(BS_USERNAME)
+                      + ":"
+                      + config.require(BS_ACCESS_KEY)
+                      + "@");
+          break;
+        case LOCAL:
+        default:
+          url = config.require(PropertiesInput.APPIUM_SERVER_URL);
+          break;
+      }
+      return new IOSDriver(new URL(url), capabilities);
     } catch (Exception e) {
       throw new RuntimeException("Failed to create IOS driver", e);
     }
