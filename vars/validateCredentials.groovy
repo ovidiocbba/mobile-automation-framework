8fb03a8745948c41c97063bc424b539b28c5af3e
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-03-31 22:26:09 -0400

    feat(ci): add shared library step to validate Jenkins credentials

---
+++ vars/validateCredentials.groovy
@@ -0,0 +1,50 @@
+def call(String execution) {
+
+    try {
+
+        def creds = [
+                usernamePassword(
+                        credentialsId: 'USERNAME',
+                        usernameVariable: 'USERNAME',
+                        passwordVariable: 'PASSWORD'
+                )
+        ]
+
+        if (execution == "browserstack") {
+
+            creds += [
+                    string(credentialsId: 'BS_USERNAME', variable: 'BS_USERNAME'),
+                    string(credentialsId: 'BS_ACCESS_KEY', variable: 'BS_ACCESS_KEY'),
+                    string(credentialsId: 'BS_APP', variable: 'BS_APP')
+            ]
+
+        }
+
+        withCredentials(creds) {
+
+            echo "Credentials validated successfully"
+
+        }
+
+    } catch (err) {
+
+        error """
+        Jenkins Credentials validation failed.
+
+        Missing credentials in Jenkins:
+
+        Required:
+        - USERNAME
+        - PASSWORD
+
+        BrowserStack required when EXECUTION=browserstack:
+        - BS_USERNAME
+        - BS_ACCESS_KEY
+        - BS_APP
+
+        Error: ${err.message}
+        """
+
+    }
+
+}
