4975600bf83e72a0aca35cd9bedb5db63e55914b
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-04-06 21:24:47 -0400

    fix(jenkins): avoid serialization issues by converting JsonSlurper results to JSON text

--- vars/getDevices.groovy
+++ vars/getDevices.groovy
@@ -1,11 +1,15 @@
 import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
 
 def call(String device, String execution) {
 
     def routerPath = "/var/jenkins_home/resources/devices/devices.json"
+
     def router = new JsonSlurper().parse(new File(routerPath))
+    router = new JsonSlurper().parseText(JsonOutput.toJson(router))
 
     def devices = new JsonSlurper().parse(new File(router[execution]))
+    devices = new JsonSlurper().parseText(JsonOutput.toJson(devices))
 
     if (device == "ALL") {
         return devices
