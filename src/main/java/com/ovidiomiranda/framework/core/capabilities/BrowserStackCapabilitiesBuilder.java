ad93e2c68cd61aa05e852481136befd74b3ef8cd
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-04-14 12:55:58 -0400

    refactor(browserstack): standardize capability configuration naming

--- src/main/java/com/ovidiomiranda/framework/core/capabilities/BrowserStackCapabilitiesBuilder.java
+++ src/main/java/com/ovidiomiranda/framework/core/capabilities/BrowserStackCapabilitiesBuilder.java
@@ -1,15 +1,15 @@
 package com.ovidiomiranda.framework.core.capabilities;
 
-import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_ACCESS_KEY;
-import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_APP;
-import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_APPIUM_LOGS;
-import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_APPIUM_VERSION;
-import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_BUILD_NAME;
-import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_DEVICE_LOGS;
-import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_NETWORK_LOGS;
-import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_PROJECT_NAME;
-import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_USERNAME;
-import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BS_VIDEO;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_ACCESS_KEY;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_APP;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_APPIUM_LOGS;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_APPIUM_VERSION;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_BUILD_NAME;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_DEVICE_LOGS;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_NETWORK_LOGS;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_PROJECT_NAME;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_USERNAME;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_VIDEO;
 import static com.ovidiomiranda.framework.core.enums.PropertiesInput.DEVICE_NAME;
 import static com.ovidiomiranda.framework.core.enums.PropertiesInput.PLATFORM_VERSION;
 
@@ -48,24 +48,25 @@ public class BrowserStackCapabilitiesBuilder {
   public void apply(final BaseOptions<?> options, final String sessionName) {
 
     // Set app uploaded to BrowserStack
-    options.setCapability("app", config.require(BS_APP));
+    options.setCapability("app", config.require(BROWSERSTACK_APP));
 
     // Build BrowserStack options map
     final Map<String, Object> bstackOptions = new HashMap<>();
-    bstackOptions.put("userName", config.require(BS_USERNAME));
-    bstackOptions.put("accessKey", config.require(BS_ACCESS_KEY));
-    bstackOptions.put("projectName", config.require(BS_PROJECT_NAME));
-    bstackOptions.put("buildName", config.require(BS_BUILD_NAME));
+    bstackOptions.put("userName", config.require(BROWSERSTACK_USERNAME));
+    bstackOptions.put("accessKey", config.require(BROWSERSTACK_ACCESS_KEY));
+    bstackOptions.put("projectName", config.require(BROWSERSTACK_PROJECT_NAME));
+    bstackOptions.put("buildName", config.require(BROWSERSTACK_BUILD_NAME));
     bstackOptions.put("sessionName", sessionName);
 
     bstackOptions.put("deviceName", config.require(DEVICE_NAME));
     bstackOptions.put("osVersion", config.require(PLATFORM_VERSION));
 
-    bstackOptions.put("video", Boolean.parseBoolean(config.require(BS_VIDEO)));
-    bstackOptions.put("deviceLogs", Boolean.parseBoolean(config.require(BS_DEVICE_LOGS)));
-    bstackOptions.put("appiumLogs", Boolean.parseBoolean(config.require(BS_APPIUM_LOGS)));
-    bstackOptions.put("networkLogs", Boolean.parseBoolean(config.require(BS_NETWORK_LOGS)));
-    bstackOptions.put("appiumVersion", config.require(BS_APPIUM_VERSION));
+    bstackOptions.put("video", Boolean.parseBoolean(config.require(BROWSERSTACK_VIDEO)));
+    bstackOptions.put("deviceLogs", Boolean.parseBoolean(config.require(BROWSERSTACK_DEVICE_LOGS)));
+    bstackOptions.put("appiumLogs", Boolean.parseBoolean(config.require(BROWSERSTACK_APPIUM_LOGS)));
+    bstackOptions.put(
+        "networkLogs", Boolean.parseBoolean(config.require(BROWSERSTACK_NETWORK_LOGS)));
+    bstackOptions.put("appiumVersion", config.require(BROWSERSTACK_APPIUM_VERSION));
     // Attach to capabilities
     options.setCapability("bstack:options", bstackOptions);
   }
