ec12c99621f3284477c8d520367ece114b436557
Ovidio Miranda <ovidiocbba@hotmail.com>
2026-04-15 22:11:56 -0400

    feat(hooks): log execution configuration and prevent duplicate logging per run

--- src/test/java/com/ovidiomiranda/framework/hooks/CommonHooks.java
+++ src/test/java/com/ovidiomiranda/framework/hooks/CommonHooks.java
@@ -1,7 +1,15 @@
 package com.ovidiomiranda.framework.hooks;
 
 import static com.ovidiomiranda.framework.core.enums.ExecutionType.BROWSERSTACK;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.APP;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.APPIUM_SERVER_URL;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.AUTOMATION_NAME;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_BUILD_NAME;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.BROWSERSTACK_PROJECT_NAME;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.DEVICE_NAME;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.FRAMEWORK_THREADS;
 import static com.ovidiomiranda.framework.core.enums.PropertiesInput.PLATFORM;
+import static com.ovidiomiranda.framework.core.enums.PropertiesInput.PLATFORM_VERSION;
 import static com.ovidiomiranda.framework.core.utils.ExecutionUtils.getExecutionType;
 import static com.ovidiomiranda.framework.utils.ScenarioUtils.getTestCaseId;
 import static com.ovidiomiranda.framework.utils.ScenarioUtils.getTestCaseTitle;
@@ -19,6 +27,7 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.qameta.allure.Allure;
 import java.io.ByteArrayInputStream;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.slf4j.Logger;
@@ -35,6 +44,7 @@ public class CommonHooks {
   private long startTime;
   private static final String SEPARATOR =
       "============================================================";
+  private static final AtomicBoolean executionInfoPrinted = new AtomicBoolean(false);
 
   private final DriverContext driverContext;
   private final DriverFactory driverFactory;
@@ -61,7 +71,7 @@ public class CommonHooks {
    *
    * @param scenario the scenario to be executed
    */
-  @Before(order = -1)
+  @Before(order = 1)
   public void beforeScenario(final Scenario scenario) {
     startTime = System.currentTimeMillis();
     LOGGER.info(SEPARATOR);
@@ -78,10 +88,42 @@ public class CommonHooks {
    */
   @Before(order = 0)
   public void setUp(final Scenario scenario) {
+
     final String platform = config.require(PLATFORM);
+    final String device = config.require(DEVICE_NAME);
+    final String platformVersion = config.require(PLATFORM_VERSION);
+    final ExecutionType executionType = getExecutionType(config);
+    final String automationName = config.require(AUTOMATION_NAME);
+
+    if (executionInfoPrinted.compareAndSet(false, true)) {
+
+      LOGGER.info(SEPARATOR);
+      LOGGER.info("Execution Type: {}", executionType);
+      LOGGER.info("Platform: {} | Version: {}", platform, platformVersion);
+      LOGGER.info("Device: {}", device);
+      LOGGER.info("Automation: {}", automationName);
+
+      LOGGER.info("App: {}", config.optional(APP));
+      LOGGER.info("Appium Server: {}", config.optional(APPIUM_SERVER_URL));
+      LOGGER.info("Threads: {}", config.optionalInt(FRAMEWORK_THREADS, 1));
+
+      LOGGER.info("OS: {}", System.getProperty("os.name"));
+      LOGGER.info("Java: {}", System.getProperty("java.version"));
+
+      LOGGER.info("Cucumber Tags: {}", System.getProperty("cucumber.filter.tags"));
+
+      if (executionType == BROWSERSTACK) {
+        LOGGER.info("BS Project: {}", config.optional(BROWSERSTACK_PROJECT_NAME));
+        LOGGER.info("BS Build: {}", config.optional(BROWSERSTACK_BUILD_NAME));
+      }
+
+      LOGGER.info(SEPARATOR);
+    }
+
     final AppiumDriver driver =
         driverFactory.createDriver(
             PlatformType.valueOf(platform.toUpperCase(ENGLISH)), getTestCaseTitle(scenario));
+
     driverContext.setDriver(driver);
   }
 
